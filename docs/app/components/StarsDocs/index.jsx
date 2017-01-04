9320bcae2e1591943b26c8f3424a3f50032569ae
Fey Chou <federica.recanatini@gmail.com>
2017-01-04 16:49:13 +0100

    Combine examples in one panel (#41)
    * Combine examples in one panel
    
    * Delete extended example

--- docs/app/components/StarsDocs/index.jsx
+++ docs/app/components/StarsDocs/index.jsx
@@ -6,36 +6,33 @@ import DocsRoot from 'components/DocsRoot';
 import {default as example} from './example.txt';
 import {default as propsDefinition} from './props-definition.txt';
 import {default as propsDefault} from './props-default.txt';
-import {default as extendedExample} from './extended-example.txt';
 
 const StarsDocs = () => (
   <div>
     <DocsRoot
-      title="Static stars"
-      component={<Stars value={2.5} />}
-      example={example}
-      propsDefinition={propsDefinition}
-      propsDefault={propsDefault} />
-
-    <br /><br />
-    <DocsRoot
-      title="Selectable stars"
+      title="Stars"
       component={
-        <div className="row">
-          <div className="col-md-3">
-            <Stars
-              value={2}
-              selectable
-              colors={[
-                '#ff464e',
-                '#fe8e17',
-                '#fec327',
-                '#7cb532',
-                '#96d04a']} />
+        <div>
+          <div className="inline-block">
+            <Stars value={2.5} />
+          </div>&nbsp;
+          <br /><br />
+          <div className="row">
+            <div className="col-md-3">
+              <Stars
+                value={2}
+                selectable
+                colors={[
+                  '#ff464e',
+                  '#fe8e17',
+                  '#fec327',
+                  '#7cb532',
+                  '#96d04a']} />
+            </div>
           </div>
         </div>
       }
-      example={extendedExample}
+      example={example}
       propsDefinition={propsDefinition}
       propsDefault={propsDefault} />
   </div>
