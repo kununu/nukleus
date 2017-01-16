12a6d228328ec01644bcee6ff42dc3778cc1b058
Kaitlin Rathwell <kaytcat@gmail.com>
2017-01-16 16:20:48 +0100

    Add tests

--- tests/Autocomplete.test.jsx
+++ tests/Autocomplete.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {mount, shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Autocomplete from 'Autocomplete'; // eslint-disable-line import/no-unresolved, import/extensions, import/no-extraneous-dependencies
 
-const autocomplete = (
+const staticAutocomplete = (
   <Autocomplete
     data={{
       items: [
@@ -28,6 +28,50 @@ const autocomplete = (
 );
 
 test('Renders Autocomplete without crashing', () => {
-  const component = renderer.create(autocomplete);
+  const component = renderer.create(staticAutocomplete);
   expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Renders no suggestions container', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'z'}});
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Renders suggestions container', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'a'}});
+  component.find('input').simulate('focus');
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Hides no suggestions on blur', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'z'}});
+  component.find('input').simulate('focus');
+  component.find('input').simulate('blur');
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Updates value on selection', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'a'}});
+  component.find('input').simulate('focus');
+  component.find('#react-autowhatever-1--item-0').simulate('click');
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Fetches suggestions on change', () => {
+  const spyFunc = jest.fn();
+  const component = mount(
+    <Autocomplete
+      data={{items: []}}
+      onGetSuggestions={spyFunc}
+      id="autocompletes"
+      label="Autocomplete"
+      name="autocomplete" />
+  );
+
+  component.find('input').simulate('change', {target: {value: 'a'}});
+  expect(spyFunc).toHaveBeenCalled();
 });
