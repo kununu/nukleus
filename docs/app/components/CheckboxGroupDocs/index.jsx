405d667b0ec8c4b2c0e569b17e1a703bdd16ca21
Catarina Lopes <clopes@ventureoak.com>
2017-01-16 15:03:50 +0000

    Fix docs

--- docs/app/components/CheckboxGroupDocs/index.jsx
+++ docs/app/components/CheckboxGroupDocs/index.jsx
@@ -11,15 +11,37 @@ const CheckboxGroupDocs = () => (
   <DocsRoot
     title="CheckboxGroup"
     component={
-      <Choice
-        name="basic"
-        onChange={() => {}}
-        checked="opA"
-        options={{
-          opA: 'Option A',
-          opB: 'Option B',
-          opC: 'Option C'
-        }} />
+      <CheckboxGroup
+        name="checkbox-group[]"
+        heading="CheckboxGroup"
+        checkboxes={
+        [
+          {
+            id: 'option-1',
+            isChecked: true,
+            label: 'option 1',
+            value: 'option-1'
+          },
+          {
+            id: 'option-2',
+            isChecked: true,
+            label: 'option 2',
+            value: 'option-2'
+          },
+          {
+            id: 'option-3',
+            isChecked: false,
+            label: 'option 3',
+            value: 'option-2'
+          },
+          {
+            id: 'option-4',
+            isChecked: false,
+            label: 'option 4',
+            value: 'option-4'
+          }
+        ]
+        } />
     }
     example={example}
     propsDefinition={propsDefinition}
--- docs/app/components/ChoiceDocs/index.jsx
+++ docs/app/components/ChoiceDocs/index.jsx
@@ -12,36 +12,15 @@ const ChoiceDocs = () => (
     title="Choice"
     component={
       <Choice
-        name="checkbox-group[]"
-        heading="CheckboxGroup"
-        checkboxes={
-        [
-          {
-            id: 'option-1',
-            isChecked: true,
-            label: 'option 1',
-            value: 'option-1'
-          },
-          {
-            id: 'option-2',
-            isChecked: true,
-            label: 'option 2',
-            value: 'option-2'
-          },
-          {
-            id: 'option-3',
-            isChecked: false,
-            label: 'option 3',
-            value: 'option-2'
-          },
-          {
-            id: 'option-4',
-            isChecked: false,
-            label: 'option 4',
-            value: 'option-4'
-          }
-        ]
-        } />
+        name="basic"
+        onChange={() => {}}
+        checked="opA"
+        options={{
+          opA: 'Option A',
+          opB: 'Option B',
+          opC: 'Option C'
+        }}
+      />
     }
     example={example}
     propsDefinition={propsDefinition}
