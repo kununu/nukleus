2546acad4f14f78d740d58d66565c0a957eb7246
Kaitlin Rathwell <kaytcat@gmail.com>
2017-01-16 18:16:29 +0100

    [www] Add autocomplete comp (#46)
    * Upgrade autocomplete library
    
    * Begin refactoring and improving wrapper and styles
    
    * Add custom footer in suggestions container
    
    * Clear no suggestions
    
    * Add scrolling behaviour for mobile and mobile container styles
    
    * Refactor to the max
    
    * Fix info styling
    
    * Default value to empty string since it is set in willMount
    
    * Change noSuggestions state name
    
    * Don’t default scrolling on mobile
    
    * Remove local state from playground app
    
    * Fix scrolling
    
    * Add todo
    
    * Remove suggestions footer
    
    * Make work with static data and fix noSuggestions box
    
    * Add scroll offset for fixed headers
    
    * Add docs
    
    * Move element positioning into utils function
    
    * add function
    
    * Movie isMobile into utils
    
    * Fix docs path to utils
    
    * Update example.txt
    
    * Update index.jsx
    
    * Update Combobox test
    
    * Add comp to readme -doh
    
    * Make default on root
    
    * Fix utils path for tests
    
    * Add basic test
    
    * Add tests

---
+++ tests/Autocomplete.test.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {mount} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Autocomplete from 'Autocomplete'; // eslint-disable-line import/no-unresolved, import/extensions, import/no-extraneous-dependencies
+
+const staticAutocomplete = (
+  <Autocomplete
+    data={{
+      items: [
+        {item: 'apple', itemInfo: 'US'},
+        {item: 'alpha', itemInfo: 'Vienna'},
+        {item: 'IBM', itemInfo: 'US'},
+        {item: 'kununu', itemInfo: 'Vienna'},
+        {item: 'kununu', itemInfo: 'US'}
+      ]
+    }}
+    value="test"
+    disabled
+    required
+    requiredLabel="Required"
+    id="autocompletes"
+    label="Autocomplete"
+    name="autocomplete"
+    placeholder="Type something..."
+    scrollOffset={70}
+    scrollTo />
+);
+
+test('Renders Autocomplete without crashing', () => {
+  const component = renderer.create(staticAutocomplete);
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Renders no suggestions container', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'z'}});
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Renders suggestions container', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'a'}});
+  component.find('input').simulate('focus');
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Hides no suggestions on blur', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'z'}});
+  component.find('input').simulate('focus');
+  component.find('input').simulate('blur');
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Updates value on selection', () => {
+  const component = mount(staticAutocomplete);
+  component.find('input').simulate('change', {target: {value: 'a'}});
+  component.find('input').simulate('focus');
+  component.find('#react-autowhatever-1--item-0').simulate('click');
+  expect(toJson(component)).toMatchSnapshot();
+});
+
+test('Fetches suggestions on change', () => {
+  const spyFunc = jest.fn();
+  const component = mount(
+    <Autocomplete
+      data={{items: []}}
+      onGetSuggestions={spyFunc}
+      id="autocompletes"
+      label="Autocomplete"
+      name="autocomplete" />
+  );
+
+  component.find('input').simulate('change', {target: {value: 'a'}});
+  expect(spyFunc).toHaveBeenCalled();
+});
