d48798aaf4a5f95d82aee71fa2379b82610fb849
feychou <federica.recanatini@gmail.com>
2017-01-17 17:18:40 +0100

    Fix docs data

--- docs/app/components/MultipleChoiceDocs/index.jsx
+++ docs/app/components/MultipleChoiceDocs/index.jsx
@@ -25,145 +25,58 @@ const MultipleChoiceDocs = () => (
             },
             {
               id: 'option-b',
-              isChecked: true,
+              isChecked: false,
               label: 'option 2',
               value: 'option-2'
-            },
-            {
-              id: 'option-c',
-              isChecked: false,
-              label: 'option 3',
-              value: 'option-2'
-            },
-            {
-              id: 'option-d',
-              isChecked: false,
-              label: 'option 4',
-              value: 'option-4'
-            }
-          ]
-          } />
+            }]} />
 
         <br />
 
         <MultipleChoice
           name="checkbox-group[]"
-          heading="MultipleChoice (with buttons)"
           inputStyle="buttons"
           checkboxes={
           [{
             id: 'option-1',
             isChecked: true,
-            label: 'flex. Arbeitszeit',
+            label: 'Option 1',
             value: 'option-1'
           },
           {
             id: 'option-2',
             isChecked: false,
-            label: 'Homeoffice',
+            label: 'Option 2',
             value: 'option-2'
           },
           {
             id: 'option-3',
             isChecked: false,
-            label: 'Kantine',
-            value: 'option-2'
+            label: 'Option 3',
+            value: 'option-3'
           },
           {
             id: 'option-4',
             isChecked: false,
-            label: 'Essenszulagen',
-            value: 'option-2'
+            label: 'Option 4',
+            value: 'option-4'
           },
           {
             id: 'option-5',
             isChecked: false,
-            label: 'Kinderbetreuung',
-            value: 'option-2'
+            label: 'Option 5',
+            value: 'option-5'
           },
           {
             id: 'option-6',
             isChecked: false,
-            label: 'Betr. Altersvorsorge',
-            value: 'option-2'
+            label: 'Option 6',
+            value: 'option-6'
           },
           {
             id: 'option-7',
             isChecked: false,
-            label: 'Barrierefreiheit',
-            value: 'option-2'
-          },
-          {
-            id: 'option-8',
-            isChecked: false,
-            label: 'Gesundheitsmaßnahmen',
-            value: 'option-2'
-          },
-          {
-            id: 'option-9',
-            isChecked: false,
-            label: 'Betriebsartzt',
-            value: 'option-4'
-          },
-          {
-            id: 'option-10',
-            isChecked: false,
-            label: 'Coaching',
-            value: 'option-4'
-          },
-          {
-            id: 'option-11',
-            isChecked: false,
-            label: 'Parkplatz',
-            value: 'option-4'
-          },
-          {
-            id: 'option-12',
-            isChecked: false,
-            label: 'gute Verkehrsanbindung',
-            value: 'option-4'
-          },
-          {
-            id: 'option-13',
-            isChecked: false,
-            label: 'Mitarbeiterrabatte',
-            value: 'option-4'
-          },
-          {
-            id: 'option-14',
-            isChecked: false,
-            label: 'Firmenwagen',
-            value: 'option-4'
-          },
-          {
-            id: 'option-15',
-            isChecked: false,
-            label: 'Mitarbeiterhandy',
-            value: 'option-4'
-          },
-          {
-            id: 'option-16',
-            isChecked: false,
-            label: 'Mitarbeiterbeteiligung',
-            value: 'option-4'
-          },
-          {
-            id: 'option-17',
-            isChecked: false,
-            label: 'Mitarbeiterevents',
-            value: 'option-4'
-          },
-          {
-            id: 'option-18',
-            isChecked: false,
-            label: 'Internetnutzung',
-            value: 'option-4'
-          },
-          {
-            id: 'option-19',
-            isChecked: false,
-            label: 'Hunde geduldet',
-            value: 'option-4'
+            label: 'Option 7',
+            value: 'option-7'
           }]} />
       </div>
     }
