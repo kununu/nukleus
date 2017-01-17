65696ef6b574f7dd625b5aea39b88fffc5d437ed
Kaitlin Rathwell <kaytcat@gmail.com>
2017-01-17 10:22:59 +0100

    Add more benefits and clean up css

--- playground/app.jsx
+++ playground/app.jsx
@@ -30,57 +30,118 @@ const App = ({location: {pathname, query}}) => (
               [{
                 id: 'option-1',
                 isChecked: true,
-                label: 'Kantine',
+                label: 'flex. Arbeitszeit',
                 value: 'option-1'
               },
               {
                 id: 'option-2',
                 isChecked: false,
-                label: 'Essenzulage',
+                label: 'Homeoffice',
                 value: 'option-2'
               },
               {
                 id: 'option-3',
                 isChecked: false,
-                label: 'Parkplatz',
+                label: 'Kantine',
                 value: 'option-2'
               },
               {
                 id: 'option-4',
                 isChecked: false,
-                label: 'Firmenwagen',
+                label: 'Essenszulagen',
                 value: 'option-2'
               },
               {
                 id: 'option-5',
                 isChecked: false,
-                label: 'Sport-/Freizeitangebote',
+                label: 'Kinderbetreuung',
                 value: 'option-2'
               },
               {
                 id: 'option-6',
                 isChecked: false,
-                label: 'Mitarbeiter Events',
+                label: 'Betr. Altersvorsorge',
                 value: 'option-2'
               },
               {
                 id: 'option-7',
                 isChecked: false,
-                label: 'Sonderkonditionen und rabatte',
+                label: 'Barrierefreiheit',
                 value: 'option-2'
               },
               {
                 id: 'option-8',
                 isChecked: false,
-                label: 'Gutscheine',
+                label: 'Gesundheitsmaßnahmen',
                 value: 'option-2'
               },
               {
                 id: 'option-9',
                 isChecked: false,
-                label: 'Fahrtkostenzuschuss',
+                label: 'Betriebsartzt',
                 value: 'option-4'
-              }]} />
+              },
+              {
+                id: 'option-10',
+                isChecked: false,
+                label: 'Coaching',
+                value: 'option-4'
+              },
+              {
+                id: 'option-11',
+                isChecked: false,
+                label: 'Parkplatz',
+                value: 'option-4'
+              },
+              {
+                id: 'option-12',
+                isChecked: false,
+                label: 'gute Verkehrsanbindung',
+                value: 'option-4'
+              },
+              {
+                id: 'option-13',
+                isChecked: false,
+                label: 'Mitarbeiterrabatte',
+                value: 'option-4'
+              },
+              {
+                id: 'option-14',
+                isChecked: false,
+                label: 'Firmenwagen',
+                value: 'option-4'
+              },
+              {
+                id: 'option-15',
+                isChecked: false,
+                label: 'Mitarbeiterhandy',
+                value: 'option-4'
+              },
+              {
+                id: 'option-16',
+                isChecked: false,
+                label: 'Mitarbeiterbeteiligung',
+                value: 'option-4'
+              },
+              {
+                id: 'option-17',
+                isChecked: false,
+                label: 'Mitarbeiterevents',
+                value: 'option-4'
+              },
+              {
+                id: 'option-18',
+                isChecked: false,
+                label: 'Internetnutzung',
+                value: 'option-4'
+              },
+              {
+                id: 'option-19',
+                isChecked: false,
+                label: 'Hunde geduldet',
+                value: 'option-4'
+              }
+              ]} />
           </div>
         </div>
 
