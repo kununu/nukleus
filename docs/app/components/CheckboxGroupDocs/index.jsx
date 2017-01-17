ae3f396be9f406da534d48e0beb77a1fda13951f
Kaitlin Rathwell <kaytcat@gmail.com>
2017-01-17 11:31:40 +0100

    Add to docs

--- docs/app/components/CheckboxGroupDocs/index.jsx
+++ docs/app/components/CheckboxGroupDocs/index.jsx
@@ -11,37 +11,161 @@ const CheckboxGroupDocs = () => (
   <DocsRoot
     title="CheckboxGroup"
     component={
-      <CheckboxGroup
-        name="checkbox-group[]"
-        heading="CheckboxGroup"
-        checkboxes={
-        [
-          {
+      <div>
+        <CheckboxGroup
+          name="checkbox-group[]"
+          heading="CheckboxGroup"
+          checkboxes={
+          [
+            {
+              id: 'option-a',
+              isChecked: true,
+              label: 'option 1',
+              value: 'option-1'
+            },
+            {
+              id: 'option-b',
+              isChecked: true,
+              label: 'option 2',
+              value: 'option-2'
+            },
+            {
+              id: 'option-c',
+              isChecked: false,
+              label: 'option 3',
+              value: 'option-2'
+            },
+            {
+              id: 'option-d',
+              isChecked: false,
+              label: 'option 4',
+              value: 'option-4'
+            }
+          ]
+          } />
+
+        <br />
+
+        <CheckboxGroup
+          name="checkbox-group[]"
+          heading="CheckboxGroup"
+          inputStyle="buttons"
+          checkboxes={
+          [{
             id: 'option-1',
             isChecked: true,
-            label: 'option 1',
+            label: 'flex. Arbeitszeit',
             value: 'option-1'
           },
           {
             id: 'option-2',
-            isChecked: true,
-            label: 'option 2',
+            isChecked: false,
+            label: 'Homeoffice',
             value: 'option-2'
           },
           {
             id: 'option-3',
             isChecked: false,
-            label: 'option 3',
+            label: 'Kantine',
             value: 'option-2'
           },
           {
             id: 'option-4',
             isChecked: false,
-            label: 'option 4',
+            label: 'Essenszulagen',
+            value: 'option-2'
+          },
+          {
+            id: 'option-5',
+            isChecked: false,
+            label: 'Kinderbetreuung',
+            value: 'option-2'
+          },
+          {
+            id: 'option-6',
+            isChecked: false,
+            label: 'Betr. Altersvorsorge',
+            value: 'option-2'
+          },
+          {
+            id: 'option-7',
+            isChecked: false,
+            label: 'Barrierefreiheit',
+            value: 'option-2'
+          },
+          {
+            id: 'option-8',
+            isChecked: false,
+            label: 'Gesundheitsmaßnahmen',
+            value: 'option-2'
+          },
+          {
+            id: 'option-9',
+            isChecked: false,
+            label: 'Betriebsartzt',
             value: 'option-4'
-          }
-        ]
-        } />
+          },
+          {
+            id: 'option-10',
+            isChecked: false,
+            label: 'Coaching',
+            value: 'option-4'
+          },
+          {
+            id: 'option-11',
+            isChecked: false,
+            label: 'Parkplatz',
+            value: 'option-4'
+          },
+          {
+            id: 'option-12',
+            isChecked: false,
+            label: 'gute Verkehrsanbindung',
+            value: 'option-4'
+          },
+          {
+            id: 'option-13',
+            isChecked: false,
+            label: 'Mitarbeiterrabatte',
+            value: 'option-4'
+          },
+          {
+            id: 'option-14',
+            isChecked: false,
+            label: 'Firmenwagen',
+            value: 'option-4'
+          },
+          {
+            id: 'option-15',
+            isChecked: false,
+            label: 'Mitarbeiterhandy',
+            value: 'option-4'
+          },
+          {
+            id: 'option-16',
+            isChecked: false,
+            label: 'Mitarbeiterbeteiligung',
+            value: 'option-4'
+          },
+          {
+            id: 'option-17',
+            isChecked: false,
+            label: 'Mitarbeiterevents',
+            value: 'option-4'
+          },
+          {
+            id: 'option-18',
+            isChecked: false,
+            label: 'Internetnutzung',
+            value: 'option-4'
+          },
+          {
+            id: 'option-19',
+            isChecked: false,
+            label: 'Hunde geduldet',
+            value: 'option-4'
+          }]} />
+      </div>
     }
     example={example}
     propsDefinition={propsDefinition}
