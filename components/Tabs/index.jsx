bb845661d7de7886765cddec9ee005a179cd1627
Daniel Marczydlo <dmarczydlo@concisesoftware.com>
2017-04-05 09:36:23 +0200

    hash prop added to tabs

--- components/Tabs/index.jsx
+++ components/Tabs/index.jsx
@@ -8,14 +8,18 @@ import {clearfix} from '../index.scss';
 export default class Tabs extends Component {
   static propTypes = {
     items: PropTypes.arrayOf(PropTypes.element).isRequired,
-    pathname: PropTypes.string.isRequired
+    pathname: PropTypes.string.isRequired,
+    hash: PropTypes.string
   };
 
   getNewProps (item) {
     const {props} = item;
     // Depending on which link it is (from react-router, from react-server, simple link) we need to access the local pathname according to the respective API
     const localPathname = props.href || props.path || props.to.pathname;
-    const newProps = localPathname === this.props.pathname ? {className: styles.active} : {};
+    const localHash = typeof props.to.hash !== 'undefined' ? props.to.hash : '';
+
+    const newProps = localPathname === this.props.pathname ? (localHash !== '' ? (localHash === this.props.hash ? {className: styles.active}: {}) : {className: styles.active}) : {};
+
     return newProps;
   }
 
