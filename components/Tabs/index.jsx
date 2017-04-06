97a601901a7fbeff13356cc64675613fcc33d387
Kaitlin Rathwell <kaytcat@gmail.com>
2017-04-06 15:55:27 +0200

    Refactor a bit

--- components/Tabs/index.jsx
+++ components/Tabs/index.jsx
@@ -11,19 +11,20 @@ export default class Tabs extends Component {
     items: PropTypes.arrayOf(PropTypes.element).isRequired,
     pathname: PropTypes.string.isRequired
   };
+
   static defaultProps = {
     hash: ''
   };
 
   getNewProps (item) {
     const {props} = item;
+    const itemHash = props.to ? props.to.hash : '';
+    const rootHash = this.props.hash;
 
     // Depending on which link it is (from react-router, from react-server, simple link) we need to access the local pathname according to the respective API
-    const hash = props.to ? props.to.hash : '';
-    const rootHash = this.props.hash ? this.props.hash : '';
     const localPathname = props.href || props.path || props.to.pathname;
 
-    const newProps = `${localPathname}${hash}` === `${this.props.pathname}${rootHash}` ? {className: styles.active} : {};
+    const newProps = `${localPathname}${itemHash}` === `${this.props.pathname}${rootHash}` ? {className: styles.active} : {};
 
     return newProps;
   }
