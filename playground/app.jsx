80b2aea5e0a5149164ef20ea71a1ccdcebd85ba6
Catarina Lopes <clopes@ventureoak.com>
2017-04-06 16:43:03 +0200

    Add disable no-console

--- playground/app.jsx
+++ playground/app.jsx
@@ -211,7 +211,7 @@ const App = ({location: {pathname, query}}) => (
               id="test"
               value="purrr"
               query={query}
-              onSelectSuggestion={suggestion => { console.log('You have chosen ', suggestion); }}
+              onSelectSuggestion={suggestion => { console.log('You have chosen ', suggestion); }} // eslint-disable-line no-console
               placeholder="Type something..."
               suggestionsFooter={<Link href="hi">No suggestions found?</Link>}
               label="Autocomplete"
