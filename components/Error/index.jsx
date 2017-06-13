bf90b73d486b65c72cc427df542ae036513ee575
Daniel Stein <stein-daniel@codingcircus.com>
2017-06-13 15:01:54 +0200

    Removed unnecessary style

--- components/Error/index.jsx
+++ components/Error/index.jsx
@@ -2,21 +2,19 @@ import React, {PropTypes} from 'react';
 
 import styles from './index.scss';
 
-const Error = ({errorStyle, info, subInfo}) => (
-  <span className={`${styles.error} ${styles[errorStyle]}`}>
+const Error = ({info, subInfo}) => (
+  <span className={`${styles.error}`}>
     <strong className={styles.info}>{info}</strong>
     {subInfo}
   </span>
 );
 
 Error.propTypes = {
-  errorStyle: PropTypes.oneOf(['inline']),
   info: PropTypes.string.isRequired,
   subInfo: PropTypes.string
 };
 
 Error.defaultProps = {
-  errorStyle: 'inline',
   subInfo: ''
 };
 
