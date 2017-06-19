12831be557a697dfc677c8a96502d02ac6720357
Daniel Stein <stein-daniel@codingcircus.com>
2017-06-19 15:11:27 +0200

    Updated comment style

--- components/utils.js
+++ components/utils.js
@@ -2,7 +2,7 @@
  * returns the current translateX of an elements in the style.transform value,
  * if string is like 'translate(-99px, 0)'
  *
- * @return {number} translateX pixel value
+ * @return {number} [translateX pixel value]
  */
 
 const translateXRegex = /translate\(([-]?[0-9]+(.[0-9]+)?)px/;
@@ -22,7 +22,7 @@ export function getTranslateXPx (elem) {
  * calculates the current right position value of the container without excl.
  * transform: translateX
  *
- * @return {number} current right withouth translateX
+ * @return {number} [current right withouth translateX]
  */
 export function getPositionRight (elem) {
   return elem.getBoundingClientRect().right - getTranslateXPx(elem);
@@ -32,7 +32,7 @@ export function getPositionRight (elem) {
  * calculates the current left position value of the container without excl.
  * transform: translateX
  *
- * @return {number} current left withouth translateX
+ * @return {number} [current left withouth translateX]
  */
 export function getPositionLeft (elem) {
   return elem.getBoundingClientRect().left - getTranslateXPx(elem);
@@ -40,6 +40,8 @@ export function getPositionLeft (elem) {
 
 /**
  * Replaces New Lines and multiple spaces and returns the new value
+ *
+ * @return {string} [updated value]
  */
 export function sanitizeWhitespace (value) {
   return value.replace(/\r\n|\r|\n| +/g, ' ');
