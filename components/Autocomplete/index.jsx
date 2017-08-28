9702d9e93823c2c0b9cd442428fba2a2b1ea89de
Dennis <dennisat@users.noreply.github.com>
2017-08-28 14:05:56 +0200

    KUNPRO-1229 Review 2 0 Display link to Create a new company also besides No results found message (#273)
    * Autocomplete accepts JSX element for noSuggestionText prop
    
    * docs update
    
    * Update version

--- components/Autocomplete/index.jsx
+++ components/Autocomplete/index.jsx
@@ -34,7 +34,10 @@ export default class Autocomplete extends React.Component {
     label: PropTypes.string.isRequired,
     labelHidden: PropTypes.bool,
     name: PropTypes.string.isRequired,
-    noSuggestionText: PropTypes.string,
+    noSuggestionText: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.element
+    ]),
     onBlur: PropTypes.func,
     onChange: PropTypes.func,
     onFocus: PropTypes.func,
