28944d44e74fe6a08b35c16c4bd0c188e5d77a0e
Daniel Stein <stein-daniel@codingcircus.com>
2017-08-31 13:23:31 +0200

    Added classes for not nested styles
    Also add prop class name to tab items

--- components/Tabs/index.jsx
+++ components/Tabs/index.jsx
@@ -5,6 +5,39 @@ import styles from './index.scss';
 
 import {clearfix} from '../index.scss';
 
+/**
+ * Remove all classes that are undefined or empty strings from array
+ *
+ * @param {Array} classNames
+ */
+function getValidClassNames (classNames) {
+  return classNames.reduce((acc, className) => {
+    if (className) {
+      acc.push(className);
+    }
+
+    return acc;
+  }, []);
+}
+
+function getTabLinkClassName (item, additionalClass) {
+  return getValidClassNames(
+    [
+      item.props.className,
+      styles.tabLink,
+      additionalClass
+    ],
+  ).join(' ');
+}
+
+function getTabItemClassName (additionalClass) {
+  return getValidClassNames(
+    [
+      styles.tabItem,
+      additionalClass
+    ]
+  ).join(' ');
+}
 
 export default class Tabs extends React.Component {
   static propTypes = {
@@ -23,14 +56,18 @@ export default class Tabs extends React.Component {
     const {props} = item;
     const itemHash = props.to && props.to.hash ? props.to.hash : '';
     const rootHash = this.props.hash;
+    console.log(props);
 
     // Depending on which link it is (from react-router, from react-server, simple link) we need to access the local pathname according to the respective API
     const localPathname = props.href || props.path || (props.to && props.to.pathname);
 
-    const newProps = `${localPathname}${itemHash}` === `${this.props.pathname}${rootHash}` ? {className: styles.active} : {};
+    const newProps = `${localPathname}${itemHash}` === `${this.props.pathname}${rootHash}` ?
+      {className: getTabLinkClassName(item, styles.active)} :
+      {className: getTabLinkClassName(item)};
 
     return newProps;
   }
+
 
   render () {
     const {items, theme} = this.props;
@@ -41,7 +78,7 @@ export default class Tabs extends React.Component {
         {items.map((item, key) => (
           <li
             key={key} // eslint-disable-line react/no-array-index-key
-            className={items.length > 1 ? '' : styles.pointerDisabled}>
+            className={items.length > 1 ? getTabItemClassName() : getTabItemClassName(styles.pointerDisabled)}>
             {React.cloneElement(item, this.getNewProps(item))}
           </li>
         ))}
