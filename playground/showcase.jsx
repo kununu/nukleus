b10cb938e6c1ad85b5a6913d87002e848ec69677
dennist <dennis.theotokatos@kununu.com>
2017-09-16 17:35:44 +0200

    stars

--- playground/showcase.jsx
+++ playground/showcase.jsx
@@ -15,15 +15,15 @@ import MultipleChoice from 'components/MultipleChoice/index';
 // import TextField from 'components/TextField/index';
 // import ToolTip from 'components/ToolTip/index';
 // import Select from 'components/Select/index';
-// import Stars from 'components/Stars/index';
-// import {
-//   controlLabel
-// } from 'components/index.scss';
+import Stars from 'components/Stars/index';
 
 import {
   multipleChoiceManyItems,
   multipleChoiceTwoItems
 } from './data/MultipleChoice';
+// import {
+//   controlLabel
+// } from 'components/index.scss';
 
 export default ({
   logo: (
@@ -97,7 +97,74 @@ export default ({
       slug: 'multipleChoice',
       title: 'Multiple choice',
       wrapperStyle: {
-        minWidth: '300px'
+        minWidth: '50%'
+      }
+    },
+    {
+      // prototype
+      center: true,
+      component: (
+        <Stars />
+      ),
+      hide: false,
+      props: [
+        {
+          forceProps: true,
+          props: {
+            name: 'test-0',
+            selectable: true,
+            value: 0
+          },
+          slug: 'simple-star-0',
+          title: 'Value 0'
+        },
+        {
+          forceProps: true,
+          props: {
+            name: 'test-1',
+            selectable: true,
+            value: 3
+          },
+          slug: 'simple-star',
+          title: 'Value 3'
+        },
+        {
+          forceProps: true,
+          props: {
+            colors: ['red', 'green', 'blue', 'tomato', 'orange'],
+            name: 'test-2',
+            selectable: true,
+            value: 3
+          },
+          slug: 'color-star',
+          title: 'Value 3 with colours'
+        },
+        {
+          forceProps: true,
+          props: {
+            name: 'test-3',
+            selectable: true,
+            value: 5
+          },
+          slug: 'color-star-5',
+          title: 'Value 5'
+        },
+        {
+          description: 'buggy :( selectable false has an issue with the size',
+          forceProps: true,
+          props: {
+            name: 'test-4',
+            selectable: false,
+            value: 5
+          },
+          slug: 'color-unselectable',
+          title: 'Value 5 unselectable'
+        }
+      ],
+      slug: 'stars',
+      title: 'Rating starts',
+      wrapperStyle: {
+        maxWidth: '50%'
       }
     }
   ]
