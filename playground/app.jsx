a8391b1bbe3e3654d6e717c79f62875d3c36bc72
Alex Khalevine <alex.khalevine@kununu.com>
2017-10-05 11:14:31 +0200

    updated grid around the presentation of the buttons

--- playground/app.jsx
+++ playground/app.jsx
@@ -571,154 +571,143 @@ const App = ({location: {pathname, query}}) => (
 
         <br />
 
-        <div className="row">
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+        <div className="flexRow">
+          <div className="flexCol">
             <Button type="default" text={<i className="fa fa-calendar" />} />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="primary" text="Primary Button" />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="secondary" text="Secondary Button" />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="info" text="Info Button" />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="danger" text="Danger Button" />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="link" text="Link Button" />
           </div>
         </div>
 
-        <br />
-
-        <div className="row">
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+        <div className="flexRow">
+          <div className="flexCol">
             <Button type="default" text="Default Button" disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="primary" text="Primary Button" disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="secondary" text="Secondary Button" disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="info" text="Info Button" disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="danger" text="Danger Button" disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="link" text="Link Button" disabled />
           </div>
         </div>
 
-        <br />
-
-        <div className="row">
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+        <div className="flexRow">
+          <div className="flexCol">
             <Button type="default" text="Default Button" outline />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="primary" text="Primary Button" outline />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="secondary" text="Secondary Button" outline />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="info" text="Info Button" outline />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="danger" text="Danger Button" outline />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="link" text="Link Button" outline />
           </div>
         </div>
 
-        <br />
-
-        <div className="row">
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+        <div className="flexRow">
+          <div className="flexCol">
             <Button type="default" text="Default Button" outline disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="primary" text="Primary Button" outline disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="secondary" text="Secondary Button" outline disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="info" text="Info Button" outline disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="danger" text="Danger Button" outline disabled />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button type="link" text="Link Button" outline disabled />
           </div>
         </div>
 
-        <br />
-
-        <div className="row">
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
-            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} />
-          </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
-            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} disabled />
-          </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
-            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} outline />
-          </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
-            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} outline disabled />
-          </div>
-        </div>
-
-        <br />
-
-        <div className="row">
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+        <div className="flexRow">
+          <div className="flexCol">
             <Button
               type="default"
               link={<Link to="/playground">Button Link</Link>} />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button
               type="primary"
               disabled
               link={<Link to="/playground">Primary Link</Link>} />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button
               type="secondary"
               link={<Link to="/playground">Secondary Link</Link>}
               outline />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button
               type="info"
               link={<Link to="/playground/huhuh">Info Link</Link>}
               disabled
               outline />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button
               type="custom"
               link={<Link to="/playground">Danger Link</Link>}
               customTheme={styles.customThemeButton} />
           </div>
-          <div className="col-lg-2 col-md-4 col-sm-6 margin-bottom-third">
+          <div className="flexCol">
             <Button
               type="link"
               link={<Link to="/playground">Link Link</Link>} />
           </div>
         </div>
 
-        <br />
+        <div className="flexRow">
+          <div className="flexCol">
+            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} />
+          </div>
+          <div className="flexCol">
+            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} disabled />
+          </div>
+          <div className="flexCol">
+            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} outline />
+          </div>
+          <div className="flexCol">
+            <Button type="custom" text="Custom Button" onClick={() => {}} customTheme={styles.customThemeButton} outline disabled />
+          </div>
+        </div>
+
 
         <div className="row">
           <div className="col-lg-12 margin-bottom-third">
