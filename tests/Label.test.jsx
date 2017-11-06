5537591cd911a663950d2014de85dcb30180c495
Kaitlin Rathwell <kaytcat@gmail.com>
2017-11-06 14:40:28 +0100

    Add label tests

---
+++ tests/Label.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Label from 'Label'; // eslint-disable-line import/no-unresolved, import/extensions, import/no-extraneous-dependencies
+
+test('Renders a label without crashing', () => {
+  const component = renderer.create(
+    <Label
+      id="2"
+      value="Test label"
+      classNames="test-class" />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Renders a title instead of a label', () => {
+  const component = renderer.create(
+    <Label
+      value="Test label"
+      classNames="test-class"
+      isTitle />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Renders a hidden label', () => {
+  const component = renderer.create(
+    <Label
+      id="2"
+      value="Test label"
+      labelHidden
+      classNames="test-class"
+      isTitle />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
