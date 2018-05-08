1b3f0a0e046a68c698c1794794960b1fa4476e12
Kaitlin Rathwell <kaytcat@gmail.com>
2018-05-08 10:48:01 +0200

    Fix linting

--- utils/elementPosition.js
+++ utils/elementPosition.js
@@ -3,8 +3,10 @@ import {isBrowser} from './executionEnvironment';
 export default function getElementPositionY (element, offset = 0) {
   if (!isBrowser) return 0;
   const box = element.getBoundingClientRect();
-  const body = document.body;
-  const docElem = document.documentElement;
+  const {
+    body,
+    docElem
+  } = document;
   const scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
   const clientTop = docElem.clientTop || body.clientTop || 0;
   return (box.top + scrollTop) - clientTop - offset;
