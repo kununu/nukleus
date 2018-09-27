aa280f7ddc2b3c18d63b9954868ccf667547677d
Kaitlin Rathwell <kaytcat@gmail.com>
2018-09-27 16:59:50 +0200

    Remove old stories stuff

--- stories/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-
-import Button from '../src/components/Button';
-
-storiesOf('Test', module)
-  .add('button', () => (
-    <Button text="hi" />
-  ));
