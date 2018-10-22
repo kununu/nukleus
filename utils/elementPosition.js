5161d1af2b422b845f25459daa5453a1be821a30
Kaitlin Rathwell <kaytcat@gmail.com>
2018-10-22 17:51:08 +0200

    Redo with correct document method

--- utils/elementPosition.js
+++ utils/elementPosition.js
@@ -4,10 +4,11 @@ export default function getElementPositionY (element, offset = 0) {
   if (!isBrowser) return 0;
   const box = element.getBoundingClientRect();
   const {
-    body
+    body,
+    documentElement
   } = document;
 
-  const scrollTop = window.pageYOffset || body.scrollTop;
-  const clientTop = body.clientTop || 0;
+  const scrollTop = window.pageYOffset || documentElement.scrollTop || body.scrollTop;
+  const clientTop = documentElement.clientTop || body.clientTop || 0;
   return (box.top + scrollTop) - clientTop - offset;
 }
