9fe49041e45848b9e97929c02be07e622d23bce5
Kaitlin Rathwell <kaytcat@gmail.com>
2018-10-22 17:43:55 +0200

    Fix bug where docElem.scrollTop is undefined

--- utils/elementPosition.js
+++ utils/elementPosition.js
@@ -4,10 +4,10 @@ export default function getElementPositionY (element, offset = 0) {
   if (!isBrowser) return 0;
   const box = element.getBoundingClientRect();
   const {
-    body,
-    docElem
+    body
   } = document;
-  const scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
-  const clientTop = docElem.clientTop || body.clientTop || 0;
+
+  const scrollTop = window.pageYOffset || body.scrollTop;
+  const clientTop = body.clientTop || 0;
   return (box.top + scrollTop) - clientTop - offset;
 }
