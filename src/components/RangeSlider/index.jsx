25fcc99242afdbb83b7613af5ddb4e6bbc7c5d1d
Alex Khalevine <alex.khalevine@kununu.com>
2019-03-25 10:42:23 +0100

    added rangeslider component

---
+++ src/components/RangeSlider/index.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Error from '../Error';
+
+import styles from './index.scss';
+
+const RangeSliderComponent = ({
+  id,
+  step,
+  min,
+  max,
+  name,
+  onBlur,
+  labelText,
+  meta: {
+    touched,
+    error,
+    value,
+  },
+  onChange,
+}) => (
+  <>
+    <div className={styles.sliderWrapper}>
+      <label htmlFor={id}>
+        {labelText}
+        <input
+          id={id}
+          type="range"
+          step={step}
+          min={min}
+          max={max}
+          name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          className={styles.slider}
+        />
+      </label>
+    </div>
+    {(touched && error) && (
+      <div className={styles.sliderErrorContainer}>
+        <Error
+          info={error}
+        />
+      </div>
+    )}
+  </>
+);
+
+RangeSliderComponent.propTypes = {
+  id: PropTypes.string.isRequired,
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  meta: PropTypes.shape({
+    value: PropTypes.number.isRequired,
+    touched: PropTypes.bool,
+    error: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.bool,
+    ]),
+  }).isRequired,
+  labelText: PropTypes.string,
+};
+
+RangeSliderComponent.defaultProps = {
+  labelText: '',
+};
+
+
+export default RangeSliderComponent;
---
+++ src/components/RangeSlider/story.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {defaultOptions} from 'defaultOptions'; // eslint-disable-line import/no-unresolved
+import {withOptions} from '@storybook/addon-options';
+import {withInfo} from '@storybook/addon-info';
+import {withKnobs} from '@storybook/addon-knobs/react';
+
+import RangeSlider from './index';
+
+storiesOf('Paginator', module)
+  .addDecorator(withOptions({...defaultOptions}))
+  .addDecorator(withKnobs)
+  .addDecorator((story, context) => withInfo('Rangeslider component! The query param needs to either be the ```window.location.search``` value or an object that contains the query params. The ``pathname`` is only necessary if you want to change the base url. The ``queryKey`` allows you to define what the url param will be - defaults to "page". Lastly, the ``baseLink`` prop allows you to use different link components from your desired library such as react-router Link or next/link from next.js. You can also just use a plain old ``<a>`` tag.')(story)(context))
+  .add('basics', () => (
+    <RangeSlider
+      id="salaryCurrencySlider"
+      min={0}
+      max={150000}
+      step={1000}
+      name="amount"
+      onChange={() => {}}
+      onBlur={() => {}}
+      meta={{
+        touched: false,
+        error: false,
+        value: 0,
+      }}
+      labelText="Select range"
+    />
+  ));
