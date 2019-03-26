a6d258b771570ad6bf74c72fd84ccfec246f9069
Alex Khalevine <alex.khalevine@kununu.com>
2019-03-26 09:19:14 +0100

    added optional ID attribute to Error component

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -3,18 +3,26 @@ import PropTypes from 'prop-types';
 
 import styles from './index.scss';
 
-const Error = ({info, subInfo}) => (
-  <span className={styles.error}>
+const Error = ({info, subInfo, id}) => (
+  <span
+    className={styles.error}
+    {...(id ? {id} : {})}
+  >
     <strong className={styles.info}>{info}</strong>
     {subInfo}
   </span>
 );
 
+
 Error.propTypes = {
   info: PropTypes.string.isRequired,
   subInfo: PropTypes.string,
+  id: PropTypes.string,
 };
 
-Error.defaultProps = {subInfo: ''};
+Error.defaultProps = {
+  subInfo: '',
+  id: null,
+};
 
 export default Error;
