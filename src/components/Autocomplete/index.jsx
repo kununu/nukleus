4b2929ac4c1292412d6d0ee22a1f1d9b6e20c91f
Kaitlin Rathwell <kaitlin.rathwell@kununu.com>
2019-06-25 14:46:13 +0200

    Always update data

--- src/components/Autocomplete/index.jsx
+++ src/components/Autocomplete/index.jsx
@@ -117,9 +117,7 @@ export default class Autocomplete extends React.Component {
     } = this.props;
     const queryObject = queryParamsToObject(query);
 
-    if (JSON.stringify(nextProps.data.items) !== JSON.stringify(data.items)) {
-      this.setState({suggestions: nextProps.data.items});
-    }
+    this.setState({suggestions: nextProps.data.items});
 
     if (nextProps.error) this.showError();
     if (!this.needsUpdate(nextProps)) return;
