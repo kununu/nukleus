2e4eb3f2c8f2f773e072816679561f4762d0d133
Kaitlin Rathwell <kaitlin.rathwell@kununu.com>
2019-07-08 14:52:51 +0200

    Refactor

--- src/components/Gauge/index.jsx
+++ src/components/Gauge/index.jsx
@@ -1,102 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './index.scss';
 
-const Gauge = () => {
-  const strokeCapOffset = 10;
-  const radius = 150;
-  const value = 100;
-  const offsetValue = value - strokeCapOffset;
-  const compValue = 20 - strokeCapOffset;
-
-  const cirumference = Math.ceil((radius * Math.PI) * 2);
-  const semiCirumference = Math.floor(cirumference / 2);
-  const semiCirumferenceValue = Math.floor((semiCirumference * offsetValue)/100);
-
+const Gauge = ({smallHand, bigHand}) => {
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.outsideWrapper}>
-          <div className={styles.svgwrapper}>
-            <svg className={styles.svgmeter}>
-              <defs>
-                <linearGradient className={styles.gradient} spreadMethod="pad" id="gradient" x1="100%" y1="0%" x2="0%" y2="0%">
-                  <stop offset="0%" />
-                  <stop offset="100%" />
-                </linearGradient>
-              </defs>
-              <circle
-                className={styles.low}
-                r="150"
-                cx="50%"
-                cy="50%"
-                strokeLinecap="butt"
-                stroke="url(#gradient)"
-                strokeWidth="60"
-                strokeDasharray={`${semiCirumference}, ${cirumference}`}
-                fill="none"
-              />
-              <circle
-                className={styles.low2}
-                r="150"
-                cx="50%"
-                cy="50%"
-                strokeLinecap="round"
-                stroke="#000000"
-                strokeWidth="50"
-                strokeDasharray={`${semiCirumferenceValue}, ${cirumference}`}
-                fill="none"
-              />
-              <circle
-                r="195"
-                cx="50%"
-                cy="50%"
-                strokeLinecap="butt"
-                stroke="#dcdcdc"
-                strokeWidth="2"
-                strokeDasharray="2, 4"
-                fill="none"
-              />
-            </svg>
-          </div>
-        </div>
-        <div className={styles.pointerWrapper}>
-          <div className={styles.pointerContainer2} style={{transform: `rotate(${compValue}deg)`}}>
-            <svg
-              className={styles.pointer2}
-              viewBox="0 0 148 515"
-            >
-              <path class="test" d="M50.4,45.3c0,0-17.8,343-22.8,388c-10.6,95.1,86.2,86.6,86.9,19.4c0.8-81.5-22.6-378-23.1-405.5
-              C90.7,13.1,52.9,8.7,50.4,45.3z"/>
-            </svg>
-            <svg
-              className={styles.arrow}
-              viewBox="0 0 528 406">
-              <path class="st0" d="M57.5,0h269.1c46.4,0,73.6,68.9,47,119.1L239,373.9c-22.8,43.2-71.2,           43.2-94.1,0L10.4,119.1
-	              C-16.1,68.9,11.1,0,57.5,0z"
-              />
-            </svg>
-          </div>
-          <div className={styles.pointerContainer} style={{transform: `rotate(${offsetValue}deg)`}}>
-            <svg
-              className={styles.pointer}
-              viewBox="0 0 148 515"
-            >
-              <path class="test" d="M50.4,45.3c0,0-17.8,343-22.8,388c-10.6,95.1,86.2,86.6,86.9,19.4c0.8-81.5-22.6-378-23.1-405.5
-              C90.7,13.1,52.9,8.7,50.4,45.3z"/>
-            </svg>
-            <svg
-              className={styles.arrow2}
-              viewBox="0 0 528 406">
-              <path class="st0" d="M57.5,0h269.1c46.4,0,73.6,68.9,47,119.1L239,373.9c-22.8,43.2-71.2,           43.2-94.1,0L10.4,119.1
-	              C-16.1,68.9,11.1,0,57.5,0z"
-              />
-            </svg>
-          </div>
-        </div>
+    <div className={styles.container}>
+      <svg
+        viewBox="0 0 275 146"
+        className={styles.meter}
+      >
+        <linearGradient
+          id="gradient"
+          gradientUnits="userSpaceOnUse"
+          x1="0"
+          y1="73"x2="275"
+          y2="73"
+        >
+          <stop 
+            offset="0"
+            style={{stopColor: '#0059CD'}}
+          />
+          <stop offset="0.2457" style={{stopColor: '#2681B0'}}/>
+          <stop offset="0.4921" style={{stopColor: '#48A695'}}/>
+          <stop offset="0.7045" style={{stopColor: '#76B96F'}}/>
+          <stop offset="0.8927" style={{stopColor: '#98C753'}}/>
+          <stop offset="0.9944" style={{stopColor: '#A5CC48'}}/>
+        </linearGradient>
+
+        <path fill="url(#gradient)" d="M51.6,146c3.1,0,5.6-2.6,5.5-5.7c0-1.1-0.1-2.2-0.1-3.3c0-44.2,35.8-80,80-80s80,35.8,80,80
+	      c0,1.1,0,2.2-0.1,3.3c-0.1,3.1,2.3,5.7,5.5,5.7h47.1c3,0,5.4-2.3,5.5-5.3c0-1.1,0-2.1,0-3.2C275,61.6,213.4,0,137.5,0
+	      S0,61.6,0,137.5c0,1.1,0,2.1,0,3.2c0.1,3,2.5,5.3,5.5,5.3H51.6z"/>
+      </svg>
+
+      <div
+        style={{transform: `rotate(${smallHand}deg)`}}
+        className={styles.smallHand}>
+        <svg
+          className={styles.arm}
+          viewBox="0 0 84 17"
+        >
+          <path
+            d="M2.5,6.4C0.4,7,0.4,9.9,3.2,9.9c2,0,72.8,6,72.8,6l0-0.1c1.8,0,3.6-0.6,5-2c2.8-2.7 2.8-7.1,0.1-9.9c-1.3-1.4-3.1-2.1-4.9-2.1l0-0.1C76.2,1.8,3.5,6.1,2.5,6.4z"
+          />
+        </svg>
       </div>
-    </>
-  )
+
+      <div
+        style={{transform: `rotate(${bigHand}deg)`}}
+        className={styles.bigHand}>
+        <svg
+          className={styles.arm}
+          viewBox="0 0 84 17"
+        >
+          <path
+            d="M2.5,6.4C0.4,7,0.4,9.9,3.2,9.9c2,0,72.8,6,72.8,6l0-0.1c1.8,0,3.6-0.6,5-2c2.8-2.7 2.8-7.1,0.1-9.9c-1.3-1.4-3.1-2.1-4.9-2.1l0-0.1C76.2,1.8,3.5,6.1,2.5,6.4z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
 };
+
+Gauge.propTypes = {
+  smallHand: PropTypes.number,
+  bigHand: PropTypes.number,
+}
+
+Gauge.defaultProps = {
+  smallHand: 30,
+  bigHand: 110,
+}
 
 export default Gauge;
