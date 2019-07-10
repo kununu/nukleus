0cd02d2d429a9ddcfaf522cd23a45f2aa4f2c967
Alex Khalevine <alex.khalevine@kununu.com>
2019-07-10 16:52:27 +0200

    CR fixes

--- src/components/RangeSlider/icons/icn-decrease-btn.jsx
+++ src/components/RangeSlider/icons/icn-decrease-btn.jsx
@@ -7,26 +7,23 @@ export default () => (
     viewBox="0 0 32 32"
   >
     <g
+      transform="translate(1, 1)"
       fill="none"
       stroke="none"
       strokeWidth="1"
       fillRule="evenodd"
     >
-      <g transform="translate(-52.000000, -822.000000)">
-        <g transform="translate(53.000000, 823.000000)">
-          <circle
-            stroke="#2186DB"
-            cx="15"
-            cy="15"
-            r="15"
-          />
-          <polygon
-            fill="#2186DB"
-            fillRule="nonzero"
-            points="12.25 13.8779297 17.6425781 13.8779297 17.6425781 15.8652344 12.25 15.8652344"
-          />
-        </g>
-      </g>
+      <circle
+        stroke="#2186DB"
+        cx="15"
+        cy="15"
+        r="15"
+      />
+      <polygon
+        fill="#2186DB"
+        fillRule="nonzero"
+        points="12.25 13.8779297 17.6425781 13.8779297 17.6425781 15.8652344 12.25 15.8652344"
+      />
     </g>
   </svg>
 );
