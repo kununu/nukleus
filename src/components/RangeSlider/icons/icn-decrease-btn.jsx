04ee39823ec7373ab687b61dc9efa7a51c3e8297
alexkhalevine <30685766+alexkhalevine@users.noreply.github.com>
2019-07-11 09:25:31 +0200

    updated range slider (#563)
    * updated range slider
    
    * updated snaps
    
    * bumped up package version
    
    * CR fixes
    
    * sonar fixes

---
+++ src/components/RangeSlider/icons/icn-decrease-btn.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default () => (
+  <svg
+    width="25px"
+    height="25px"
+    viewBox="0 0 32 32"
+  >
+    <g
+      transform="translate(1, 1)"
+      fill="none"
+      stroke="none"
+      strokeWidth="1"
+      fillRule="evenodd"
+    >
+      <circle
+        stroke="#2186DB"
+        cx="15"
+        cy="15"
+        r="15"
+      />
+      <polygon
+        fill="#2186DB"
+        fillRule="nonzero"
+        points="12.25 13.8779297 17.6425781 13.8779297 17.6425781 15.8652344 12.25 15.8652344"
+      />
+    </g>
+  </svg>
+);
---
+++ src/components/RangeSlider/icons/icn-increase-btn.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default () => (
+  <svg
+    width="25px"
+    height="25px"
+    viewBox="0 0 32 32"
+  >
+    <g
+      transform="translate(1, 1)"
+      fill="none"
+      stroke="none"
+      strokeWidth="1"
+      fillRule="evenodd"
+    >
+      <circle
+        stroke="#2186DB"
+        cx="15"
+        cy="15"
+        r="15"
+      />
+      <polygon
+        fill="#2186DB"
+        fillRule="nonzero"
+        points="9.56445312 16.2949219 9.56445312 14.4902344 14.2480469 14.4902344 14.2480469 9.77441406 16.0849609 9.77441406 16.0849609 14.4902344 20.7685547 14.4902344 20.7685547 16.2949219 16.0849609 16.2949219 16.0849609 21 14.2480469 21 14.2480469 16.2949219"
+      />
+    </g>
+  </svg>
+);
